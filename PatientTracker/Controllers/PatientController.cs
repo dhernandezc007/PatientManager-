db86f0da76d096a84493e3e99f31e8d28dffc7e5
Danny Hernández <162759582+dhernandezc007@users.noreply.github.com>
2025-12-26 13:57:27 -0600

    Initial commit: Patient Manager Web App

---
+++ PatientTracker/Controllers/PatientController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PatientTracker.Models;
+
+namespace PatientTracker.Controllers
+{
+    public class PatientController : Controller
+    {
+        private static List<PatientModel> patients = new List<PatientModel>();
+        private static List<PatientModel> criticalPatients = new List<PatientModel>();
+
+		private static int nextId = 1;
+
+
+		//Return and populate a ListOfPatients View with the list of all patients created
+		public IActionResult ListOfPatients()
+        {
+            return View(patients);
+        }
+
+        //Return and populate a ListOfCriticalPatients View with the list of only critical patients 
+        public IActionResult ListOfCriticalPatients()
+        {
+			var criticals = patients.Where(p =>
+				  p.headachePainValue == 10 ||
+				  p.chestPainValue == 10 ||
+				  p.muscleAcheValue == 10).ToList();
+
+			return View(criticals);
+		}
+
+        //Return the CreatePatient View
+        [HttpGet]
+        public IActionResult CreatePatient()
+        {
+          return View();
+        }
+
+        //Write a method to take the new Patient object from the CreatePatient View and add it to the list of patients.
+        [HttpPost]
+		public IActionResult CreatePatient(PatientModel patient)
+		{
+			if (ModelState.IsValid)
+			{
+				patient.Id = nextId++;
+				patients.Add(patient);
+
+				return RedirectToAction("ListOfPatients");
+			}
+			return View(patient);
+		}
+
+	}
+}
---
+++ PatientTracker/Models/PatientModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PatientTracker.Models
+{
+    public class PatientModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-Za-z]{3,10}$", ErrorMessage = "First name must be between 3 and 10 characters long and contain only letters.")]
+		public string FirstName { get; set; }
+
+        [Required]
+		[RegularExpression(@"^[A-Za-z]{3,10}$", ErrorMessage = "Last name must be between 3 and 10 characters long and contain only letters.")]
+
+		public string LastName { get; set; }
+        public bool headacheExists { get; set; }
+        public int headachePainValue { get; set; }
+        public bool chestpainExists { get; set; }
+        public int chestPainValue { get; set; }
+        public bool muscleacheExists { get; set; }
+        public int muscleAcheValue { get; set; }
+    }
+}
